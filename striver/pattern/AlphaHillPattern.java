955671abbf04f76b895f3ce60b9e4301ab043097
92Mohammad <m.e89571828@gmail.com>
2024-11-14 17:18:43 +0530

    solve AlphaHillPattern.java

---
+++ striver/pattern/AlphaHillPattern.java
@@ -0,0 +1,23 @@
+package pattern;
+
+public class AlphaHillPattern {
+    public static void main(String[] args) {
+        alphaHillPattern(5);
+    }
+    public static void alphaHillPattern(int n) {
+        for (int i = 1; i <= n; i++){
+            // print space
+            for (int s = 1; s <= (n - i); s++){
+                System.out.print(" ");
+            }
+            // print letter
+            int letter = 64;
+            for(int l = 1; l <= 2*i -1; l++){
+                if (l > i) letter--;
+                else letter++;
+                System.out.print((char)letter + " ");
+            }
+            System.out.println();
+        }
+    }
+}
