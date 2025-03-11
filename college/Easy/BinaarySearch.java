11d65e2459f4b936028a5be3a5056acc6cd523c2
92Mohammad <m.e89571828@gmail.com>
2025-03-11 23:50:50 +0530

    binary search

--- college/Easy/BinaarySearch.java
+++ college/Easy/BinaarySearch.java
@@ -2,10 +2,14 @@ package Easy;
 
 public class BinaarySearch {
     public static void main(String[] args) {
-        // int[] nums = {1, 2, 2, 3, 4, 10};
-        // System.out.println("ans: " + binarySearch(nums, 2));
-        int[] nums = {2,5,6,0,0,1,2};
-        System.out.println("Ans: " + findPivotIndexInRotatedSortedArray(nums));
+        
+        // int[] nums = {2,5,6,0,0,1,2};
+        int[] nums1 = {4, 5, 6, 6, 7, 7, 0, 1, 2, 3};
+
+        System.out.println("Ans: " + findPivot(nums1));
+
+        // 4,5,6,7,0,1,2
+        // 
         
     }
     public static int binarySearch(int[] nums, int target) {
@@ -18,22 +22,18 @@ public class BinaarySearch {
         }
         return -1;
     }
-
-    public static int findPivotIndexInRotatedSortedArray(int[] nums) {
-        int s = 0, e = nums.length - 1;
-        while (s <= e) {
-            int m = s + (e - s) / 2;
-            if (nums[m] >= (m - 1 < 0 ? nums[m] : nums[m - 1]) && nums[m] > (m + 1 == nums.length ? nums[m] : nums[m + 1])) return m;
-            if (nums[m] >= nums[s] && nums[s] >= nums[e]) {
-                // you are in first part of the array
-                s = m + 1;
-            }
-            else  {
-                // you are in second part of the array
-                // move left
-                e = m - 1;
-            }
+    public static int findPivot(int[] nums) {
+        int n = nums.length;
+        if (n == 1) return 0;
+        if ((nums[0] == nums[n - 1] ) && nums[0] > nums[1]) return 0;
+        
+        int s = 0, e = n - 1;
+        while (s < e) {
+            int m = s + (e - s)  /2;
+            if (nums[m] > (m - 1 < 0 ? Integer.MIN_VALUE : nums[m - 1]) && nums[m] > (m + 1 == n ? Integer.MIN_VALUE : nums[m + 1])) return m;
+            else if (nums[m] >= nums[0] && nums[s] >= nums[n - 1]) s = m + 1;
+            else e = m - 1;
         }
-        return -1;
+        return s;
     }
 }
