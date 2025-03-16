24c2841f9dd2909d832bbe7c783a0c1f58178289
92Mohammad <m.e89571828@gmail.com>
2025-03-16 23:06:03 +0530

    easy: realative sort array, number of good pairs, largest positive integer that exist with its negative

---
+++ college/Easy/RelativeSortArray.java
@@ -0,0 +1,70 @@
+package Easy;
+
+import java.util.Arrays;
+// https://leetcode.com/problems/relative-sort-array/description/  [SOLVED]
+
+public class RelativeSortArray {
+    public static void main(String[] args) {
+        int[] arr1 = { 28, 6, 22, 8, 44, 17 };
+        int[] arr2 = { 22, 28, 8, 6 };
+        // int[] arr01 = {2,3,1,3,2,4,6,7,9,2,19};
+        // int[] arr02 = {2,1,4,3,9,6};
+
+        System.out.println(Arrays.toString(relativeSortArray(arr1, arr2)));
+
+    }
+
+    public static int[] relativeSortArray(int[] arr1, int[] arr2) {
+        int[] freq = new int[1001];
+        int[] res = new int[arr1.length];
+        int max = arr1[0];
+        for (int num : arr1) {
+            max = Math.max(max, num);
+            freq[num]++;
+        }
+        int i = 0;
+        for (int num : arr2) {
+            while (freq[num] != 0) {
+                res[i++] = num;
+                freq[num]--;
+            }
+        }
+        // add the remaining one
+        int s = freq[arr2[arr2.length - 1]]; // 6
+        for (int j = s; j <= max; j++) {
+            if (freq[j] > 0) {
+                while (freq[j] != 0) {
+                    res[i++] = j;
+                    freq[j]--;
+                }
+            }
+        }
+        return res;
+    }
+
+    public static int[] relativeSortArray1(int[] arr1, int[] arr2) {
+        int[] freq = new int[1001];
+        int[] res = new int[arr1.length];
+        int max = arr1[0];
+        for (int num : arr1) {
+            max = Math.max(max, num);
+            freq[num]++;
+        }
+        int i = 0;
+        for (int num : arr2) {
+            while (freq[num] != 0) {
+                res[i++] = num;
+                freq[num]--;
+            }
+        }
+        // add the remaining one
+        int s = freq[arr2[arr2.length - 1]]; // 6
+        for (int j = s; j <= max; j++) {
+            while (freq[j] != 0) {
+                res[i++] = j;
+                freq[j]--;
+            }
+        }
+        return res;
+    }
+}
