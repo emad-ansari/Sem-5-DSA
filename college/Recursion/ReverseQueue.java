b0e1294f49d8ae1e66c7776d0e8f453304c44dca
92Mohammad <m.e89571828@gmail.com>
2025-03-28 23:16:51 +0530

    updated: reverse queue using stack

--- college/Recursion/ReverseQueue.java
+++ college/Recursion/ReverseQueue.java
@@ -1,10 +1,21 @@
 package Recursion;
 
+import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 //https://www.geeksforgeeks.org/problems/queue-reversal/1  [SOLVED]
 public class ReverseQueue {
     public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+        queue.add(4);
+        queue.add(5);
+
+        System.out.println(queue);
+        System.out.println(reverseQueueUsingStack(queue));
         
     }
     public Queue<Integer> reverseQueue(Queue<Integer> queue) {
@@ -19,8 +30,15 @@ public class ReverseQueue {
     }
 
     // Reverse queue using stack [TODO]
-    // public static Queue<Integer> reverseQueueUsingStack(Queue<Integer> queue) {
-        
-    // }
+    public static Queue<Integer> reverseQueueUsingStack(Queue<Integer> queue) {
+        Stack<Integer> stack = new Stack<>();
+        while (!queue.isEmpty()) {
+            stack.push(queue.poll());
+        }
+        while (!stack.isEmpty() ) {
+            queue.offer(stack.pop());
+        }   
+        return queue;
 
+    }
 }
