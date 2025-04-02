6189abe429da6197775dc72b7f205e1b299cfb4c
92Mohammad <m.e89571828@gmail.com>
2025-04-02 19:37:41 +0530

    medium: optimized rotate array

--- college/medium/RotateArray.java
+++ college/medium/RotateArray.java
@@ -17,5 +17,26 @@ public class RotateArray {
         int indx = (i + k) % nums.length;
         nums[indx] = l;
     }
+
+    // OPTIMIZED ONE
+    public static void rotate1(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+        helper(nums, 0, n - 1);
+        helper(nums, 0, k - 1);
+        helper(nums, k, n - 1);
+    }
+    public static void helper1(int[] a , int l, int r) {
+        while (l <= r) {
+            swap(a, l, r);
+            l++;
+            r--;
+        }
+    }
+    public static void swap(int[] nums, int f, int s) {
+        int temp = nums[f];
+        nums[f] = nums[s];
+        nums[s] = temp;
+    }
     
 }
