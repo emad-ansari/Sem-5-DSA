ca9ecbd0e5c58428e717f05afb6d3ef1f5d25158
92Mohammad <m.e89571828@gmail.com>
2025-04-06 15:56:35 +0530

    optimize: next permutation

--- college/medium/NextPermutation.java
+++ college/medium/NextPermutation.java
@@ -1,12 +1,12 @@
 package medium;
 
-import java.util.Arrays;
 // https://leetcode.com/problems/next-permutation/description/ [SOLVED]
 public class NextPermutation {
     public static void main(String[] args) {
         
     }
-    // NOT OPTIMIZED
+
+    // OPTIMIZED ONE
 
     public void nextPermutation(int[] nums) {
         int n = nums.length;
@@ -15,7 +15,7 @@ public class NextPermutation {
         while (i >= 0 && nums[i] >= nums[i + 1]) i--;
         // sort the array from i + 1 to n - 1; and return
         if (i < 0) {
-            Arrays.sort (nums, i + 1, n);
+            reverse(nums, i + 1, n -1);
             return;
         }
         // find next greater element
@@ -23,16 +23,26 @@ public class NextPermutation {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
-        Arrays.sort(nums, i + 1, n);
+        // Arrays.sort(nums, i + 1, n);
+        reverse(nums, i + 1, n -1);
         // sort from i + 1 to n - 1;
     }
-    private static int search (int[] nums, int s , int e, int t) {
+    private static int search(int[] nums, int s , int e, int t) {
         while (s <= e) {
             int m = s + (e - s) / 2;
             if (t < nums[m]) s = m + 1;
             else e = m - 1;
         }
         return e;
+    }
+    public void reverse(int[] nums, int s, int e) {
+        while (s <= e) {
+            int temp = nums[s];
+            nums[s] = nums[e];
+            nums[e] = temp;
+            s++;
+            e--;
+        }
     }
     
 }
