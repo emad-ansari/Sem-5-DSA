3111bb78d0a625cd5b1f3ab59e4581bb2ce9145b
92Mohammad <m.e89571828@gmail.com>
2025-04-23 20:49:25 +0530

    dp: subset sum, 0/1 knapsack

---
+++ college/dp/Knapsack.java
@@ -0,0 +1,51 @@
+package dp;
+
+public class Knapsack {
+    public static void main(String[] args) {
+        int[] p = {10, 40, 30, 50};
+        int[] wt = {5, 4, 2, 3};
+        System.out.println(fn(p, wt, 5));
+    }
+    static int knapsack(int W, int val[], int wt[]) {
+        // code here
+        int n = val.length;
+        int[][] dp = new int[n + 1][W + 1];
+        for (int[] arr: dp) {
+            for (int i = 0; i < arr.length; i++) arr[i] = -1;
+        }
+        return fxn(val, wt, val.length, W, dp);   
+        
+    }
+    // Memoization
+    public static int fxn(int[] val, int[] wt, int i, int c, int[][] dp) {
+        if (i == 0 || c <= 0) return 0;
+        
+        if (dp[i - 1][c] != -1) return dp[i - 1][c];
+        int pick = Integer.MIN_VALUE;
+        if (c >= wt[i - 1]) {
+            pick = val[i - 1] + fxn(val, wt, i - 1, c - wt[i - 1], dp);
+        }
+        int nonPick = fxn(val, wt, i - 1, c, dp);
+        return dp[i - 1][c] = Math.max(pick, nonPick);
+    }
+
+    // Tabulation
+    public static int fn(int[] val , int[] wt, int c) {
+        int n = val.length;
+        int[][] dp = new int[n + 1][c + 1];
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= c; j++) {
+                int pick = 0;
+                if (j >= wt[i - 1]) {
+                    pick = val[i - 1] + dp[i - 1][j - wt[i -1]];
+                }
+                int nonPick = dp[i - 1][j];
+                dp[i][j] = Math.max(pick, nonPick);
+            }
+        }
+        return dp[n][c];
+    }
+
+    
+}
---
+++ college/dp/SumSubset.java
@@ -0,0 +1,27 @@
+package dp;
+
+// https://www.geeksforgeeks.org/problems/subset-sum-problem-1611555638/1 [SOLVED]
+
+public class SumSubset{
+
+    static Boolean isSubsetSum(int arr[], int sum) {
+        // code here
+        int n = arr.length;
+        Boolean[][] dp = new Boolean[n + 1][sum + 1];
+        for (Boolean[] num: dp) {
+            for (int j = 0; j < num.length; j++) num[j] = null;
+        }
+        return fxn(arr, 0, sum, dp);
+    }
+    public static Boolean fxn(int[] arr, int i, int t, Boolean[][] dp) {
+        if (t == 0) return true;
+        if (i == arr.length) return false;
+        if (dp[i][t] != null) return dp[i][t];
+        boolean pick = false;
+        if (t >= arr[i]) {
+            pick = fxn(arr, i + 1, t - arr[i], dp);
+        }
+        boolean nonPick = fxn(arr, i + 1, t, dp);
+        return dp[i][t] = (pick || nonPick);
+    }
+}
