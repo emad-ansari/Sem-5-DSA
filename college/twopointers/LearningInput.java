89ef66991eaf71737158ecb56a8322420c25a00e
92Mohammad <m.e89571828@gmail.com>
2025-06-29 13:24:34 +0530

    two-pointers: Number of Subsequences That Satisfy the Given Sum Condition

---
+++ college/twopointers/LearningInput.java
@@ -0,0 +1,31 @@
+package twopointers;
+import java.util.*;
+
+public class LearningInput {
+    public static void main(String[] args) {
+        List<List<Integer>> list  = new LinkedList<>();
+
+        List<Integer> result = new LinkedList<>();
+
+        result.add(1);
+        result.add(2);
+        result.add(3);
+        list.add(result);
+        List<Integer> ans2 = new LinkedList<>();
+        ans2.add(3);
+        ans2.add(5);
+        ans2.add(10);
+        list.add(ans2);
+
+
+        // Print using deepToString
+        System.out.println((list));
+    }
+
+    public static void printOutput(int[] nums){
+        for (int num: nums){
+            System.out.print(num + " ");
+        }
+    }
+
+}
---
+++ college/twopointers/Leetcode1498.java
@@ -0,0 +1,29 @@
+package twopointers;
+import java.util.*;
+// https://leetcode.com/problems/number-of-subsequences-that-satisfy-the-given-sum-condition/
+
+public class Leetcode1498  {
+
+
+   int M = (int)1e9 + 7;
+    public int numSubseq(int[] nums, int target) {
+        Arrays.sort(nums);
+        int n = nums.length, l = 0, r = n - 1;
+        int cnt = 0;
+        int[] pow2 = new int[n + 1];
+        pow2[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            pow2[i] = (pow2[i - 1] * 2) % M;
+        }
+
+        while (l <= r) {
+            if (nums[l] + nums[r] <= target) {
+                int p = r - l;
+                cnt = (cnt + pow2[r - l]) % M;
+                l++;
+            }
+            else r--;
+        }
+        return cnt;
+    }
+}
