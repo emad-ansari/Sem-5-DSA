34678a55da69e84fdd790fb28916e5128a4acbbb
92Mohammad <m.e89571828@gmail.com>
2025-08-17 16:33:48 +0530

    added tabulation approach

--- college/dp/PartitionEqualSubsetSum.java
+++ college/dp/PartitionEqualSubsetSum.java
@@ -7,9 +7,7 @@ public class PartitionEqualSubsetSum {
         int[] nums = {23,13,11,7,6,5,5};
         System.out.println(canPartition(nums));
         // System.out.println(cnt);
-        System.out.println(count);
-
-        
+        System.out.println(count);   
     }
     public static boolean canPartition(int[] nums) {
         int ts = 0, n = nums.length;
@@ -36,7 +34,7 @@ public class PartitionEqualSubsetSum {
     //memoization
     public static int fxn(int[] nums, int i, int ts, int[][] dp) {
         if (ts == 0) return 1;
-        if (i == nums.length || ts < 0) return 0;;
+        if (i == nums.length || ts < 0) return 0;
 
         if (dp[i][ts] != -1) return dp[i][ts];
         
@@ -46,4 +44,33 @@ public class PartitionEqualSubsetSum {
         int not_pick = fxn(nums, i + 1, ts, dp);
         return dp[i][ts] = not_pick;
     }
+
+    public static boolean canPartitionWithTab(int[] nums) {
+        int n = nums.length, sum = 0;
+ 
+        for (int num: nums) sum += num;
+
+        if (sum % 2 != 0) return false;
+
+        int targetSum = sum / 2;
+
+        boolean[][] dp = new boolean[n + 1][targetSum + 1];
+
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = true;
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int t = 0; t <= targetSum; t++) {
+                boolean pick = false;
+                if (nums[i] <= t) {
+                    pick = dp[i + 1][t - nums[i]];
+                }
+                dp[i][t] = pick || dp[i + 1][t];
+            }
+        }
+
+        return dp[0][targetSum];
+    }
+
 }
