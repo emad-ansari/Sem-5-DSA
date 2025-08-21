a7881e291d210f3fa2ac7a233199ffe958e77a75
92Mohammad <m.e89571828@gmail.com>
2025-08-21 23:07:04 +0530

    add: space optimization & get longest increasing subseq

--- college/dp/LongestIncreasingSubsequence.java
+++ college/dp/LongestIncreasingSubsequence.java
@@ -1,10 +1,15 @@
 package dp;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Collections;
 
 public class LongestIncreasingSubsequence {
     public static void main(String[] args) {
         int[] nums = {10,9,2,5,3,7,101,18};
-        System.out.println(lengthOfLIS(nums));
+        // System.out.println(lis(nums, 0, -1, new ArrayList<>()));
+        System.out.println(longestIncSubseq(nums));
+        System.out.println(longestSubseq(nums));
         
     }
     public static int lengthOfLIS(int[] nums) {
@@ -54,5 +59,61 @@ public class LongestIncreasingSubsequence {
         }
         return dp[n - 1][n];
     }
-    
+
+    // space optimization.
+    public static int longestIncSubseq(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        int max = 0;
+        for (int num: dp) {
+            max = Math.max(max, num);
+        }
+        return max;
+    }
+
+    // restoring subsequence.
+    public static List<Integer> longestSubseq(int[] nums) {
+        List<Integer> subseq = new ArrayList<>();
+
+        int n = nums.length;
+        int[] dp = new int[n];
+        int[] p = new int[n];
+
+        Arrays.fill(dp, 1);
+        Arrays.fill(p, -1);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i] && dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1;
+                    p[i] = j;
+                }
+            }
+        }
+
+        int max = 0;
+        int maxIndx = 0;
+        for (int i = 0; i < n; i++) {
+            if (dp[i] > max) {
+                max = dp[i];
+                maxIndx = i;
+            }
+        }
+        
+        while(maxIndx !=  -1) {
+            subseq.add(nums[maxIndx]);
+            maxIndx = p[maxIndx];
+        }
+        Collections.reverse(subseq);
+        return subseq;
+    }
 }
